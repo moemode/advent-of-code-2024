daa3e431bd1baa7753a903eab1a887774383cfd0
moemode <pfeffer.moritz@gmail.com>
2024-12-04 20:56:28 +0000

    Document and rename day 1 part 1 solution

--- day01a/src/main.rs
+++ day01a/src/main.rs
@@ -1,4 +1,8 @@
-fn diff(l: &mut Vec<i64>, r: &mut Vec<i64>) -> i64 {
+
+/// Computes the total distance between two lists by pairing up the smallest
+/// numbers in each list, then the second-smallest, and so on, and summing the
+/// absolute differences of each pair.
+fn total_distance(l: &mut Vec<i64>, r: &mut Vec<i64>) -> i64 {
     l.sort_unstable();
     r.sort_unstable();
     l.iter().zip(r.iter()).map(|(a, b)| (a - b).abs()).sum()
@@ -15,5 +19,5 @@ fn main() {
         a.push(atoi::atoi::<i64>(&line[0..num_len]).unwrap());
         b.push(atoi::atoi::<i64>(&line[num_len + 3..]).unwrap());
     }
-    println!("{}", diff(&mut a, &mut b));
+    println!("{}", total_distance(&mut a, &mut b));
 }
